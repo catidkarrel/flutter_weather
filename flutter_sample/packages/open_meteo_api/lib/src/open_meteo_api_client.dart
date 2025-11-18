84954489aebfce976727b166f2ac45ba9e0d32b0
catidkarrel <karrel@mobileit.co.jp>
2025-11-18 17:32:50 +0900

    weather model implementation

--- flutter_sample/packages/open_meteo_api/lib/src/open_meteo_api_client.dart
+++ flutter_sample/packages/open_meteo_api/lib/src/open_meteo_api_client.dart
@@ -42,32 +42,36 @@ class OpenMeteoApiClient {
   if (results.isEmpty) throw LocationNotFoundFailure();
 
   return Location.fromJson(results.first as Map<String, dynamic>);
-}
-
-Future<Weather> getWeather({
-  required double latitude,
-  required double longitude,
-}) async {
-  final weatherRequest = Uri.https(_baseUrlWeather, 'v1/forecast', 
-  {'latitude': '$latitude',
-   'longitude': '$longitude',
-   'current_weather': 'true'
-  });
-
-  final weatherResponse = await _httpClient.get(weatherRequest);
-
-  if (weatherResponse.statusCode != 200) {
-    throw WeatherRequestFailure();
   }
 
-  final bodyJson = jsonDecode(weatherResponse.body) as Map<String, dynamic>;
+  Future<Weather> getWeather({
+    required double latitude,
+    required double longitude,
+  }) async {
+    final weatherRequest = Uri.https(_baseUrlWeather, 'v1/forecast', 
+    {'latitude': '$latitude',
+    'longitude': '$longitude',
+     'current_weather': 'true'
+    });
 
-  if (!bodyJson.containsKey('current_weather')) {
-    throw WeatherNotFoundFailure();
+    final weatherResponse = await _httpClient.get(weatherRequest);
+
+    if (weatherResponse.statusCode != 200) {
+     throw WeatherRequestFailure();
+    }
+
+    final bodyJson = jsonDecode(weatherResponse.body) as Map<String, dynamic>;
+
+    if (!bodyJson.containsKey('current_weather')) {
+      throw WeatherNotFoundFailure();
+    }
+
+    final weatherJson = bodyJson['current_weather'] as Map<String, dynamic>;
+
+    return Weather.fromJson(weatherJson);
   }
 
-  final weatherJson = bodyJson['current_weather'] as Map<String, dynamic>;
-
-  return Weather.fromJson(weatherJson);
-}
+  void close() {
+    _httpClient.close();
+  }
 }
