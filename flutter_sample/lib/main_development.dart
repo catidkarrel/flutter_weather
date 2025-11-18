bcb9d77a77edf29e29a8960df290517e23bb3e7f
catidkarrel <karrel@mobileit.co.jp>
2025-11-18 14:49:56 +0900

    init commit

---
+++ flutter_sample/lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_sample/app/app.dart';
+import 'package:flutter_sample/bootstrap.dart';
+
+Future<void> main() async {
+  await bootstrap(() => const App());
+}
---
+++ flutter_sample/packages/open_meteo_api/test/open_meteo_api_test.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:open_meteo_api/open_meteo_api.dart';
+
+void main() {}
